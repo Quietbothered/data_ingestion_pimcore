f6bb9491996a908e4670a50bb04f81504b22e036
aditya <you@example.com>
2026-01-16 12:42:48 +0530

    Removed old and unnecessary code

--- app/utils/generate_ingestion_id.py
+++ app/utils/generate_ingestion_id.py
@@ -8,12 +8,6 @@ info_logger = LoggerFactory.get_info_logger()
 error_logger = LoggerFactory.get_error_logger()
 debug_logger = LoggerFactory.get_debug_logger()
 
-# [ OLD CODE ]
-# def generate_ingestion_id(file_path: str, file_type: str) -> str:
-#     info_logger.info(f"generate_ingestion_id | generate predictable ingestion id based on file name")
-#     raw = f"{file_path}|{file_type}"
-#     return hashlib.sha256(raw.encode()).hexdigest()
-
 class GenerateFileAndIngestionID:
     @staticmethod
     def generate_file_id(file_path: str, file_type: str) -> str:
